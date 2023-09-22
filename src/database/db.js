d6831c0b1629a4b40e3322ca12ee1f82c6f23c1e
JuanpaCortez93 <63519906+JuanpaCortez93@users.noreply.github.com>
2023-09-22 15:20:58 -0500

    update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise";
-import {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} from '../config.js'
+import {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} from '../config.js'
 
 export const connection = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    port: 3306,
+    port: DB_PORT,
     database: DB_NAME
 });
